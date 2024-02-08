b4668cbb743a759f7076035e44f0eaa7a28c9780
donnchadofmunster <131806948+donnchadofmunster@users.noreply.github.com>
2024-02-08 15:59:17 +0000

    RF Initial Commit + Bidirectional Data Transfer
    Transfers "John" from one RF transceiver to the other upon a button press, then once sent the receiving Pico will turn its LED off
    LEDs on pin 15, button on pin 14
    As of now, LEDs currently do not work, RF untested due to lack of antennae

---
+++ RF/src/NRF24.cpp
@@ -0,0 +1,136 @@
+#include "NRF24.h"
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+#include <string.h>
+#include <pico/time.h>
+
+
+NRF24::NRF24(spi_inst_t *port, uint16_t csn, uint16_t ce)
+{
+    this->port = port;
+    this->csn = csn;
+    this->ce = ce;
+
+    spi_init(this->port, 1000000 );
+    gpio_set_function(20, GPIO_FUNC_SPI); // Serial Clock
+    gpio_set_function(18, GPIO_FUNC_SPI); // Serial Data Into NRF
+    gpio_set_function(19, GPIO_FUNC_SPI); // Serial Data Out of NRF
+
+    gpio_init(csn);
+    gpio_init(ce);
+
+    gpio_set_dir(csn, 1);
+    gpio_set_dir(ce, 1);
+
+    ceLow();
+    csnHigh();
+
+}
+
+NRF24::~NRF24()
+{
+}
+
+uint8_t NRF24::readReg(uint8_t reg){
+    uint8_t result = 0;
+    reg = ( 0b00011111 & reg);
+    csnLow();
+    spi_write_blocking(port, &reg,1);
+    spi_read_blocking(port, 0xff,&result, 1);
+    csnHigh();
+
+    return result;
+}
+
+void NRF24::writeReg(uint8_t reg, uint8_t data){
+    writeReg(reg, &data, 1);
+}
+void NRF24::writeReg(uint8_t reg, uint8_t *data, uint8_t size){
+    reg = 0b00100000 | ( 0b00011111 & reg);
+    csnLow();
+    spi_write_blocking(port, &reg, 1);
+    spi_write_blocking(port, (uint8_t*)data, size);
+    csnHigh();
+}
+
+void NRF24::config(){
+    csnHigh();
+    ceLow();
+    sleep_ms(11);
+
+    writeReg(0, 0b00001010); // config.
+    sleep_us(1500);
+
+    writeReg(1,0); // no ack.
+
+    writeReg(5, 60); // channel.
+
+    writeReg(0x0a, (uint8_t*)"gyroc",5);
+    writeReg(0x10, (uint8_t*)"gyroc",5);
+
+    writeReg(0x11, 32);
+
+    
+
+}
+
+void NRF24::modeTX(){
+    uint8_t reg = readReg(0);
+    reg &= ~(1<<0);
+    writeReg(0, reg);
+    
+    ceLow(); // Correction from video, thansk schuhmann.
+    
+    sleep_us(130);
+}
+void NRF24::modeRX(){
+    uint8_t reg = readReg(0);
+    reg |= (1<<0);
+    writeReg(0, reg);
+    
+    ceHigh(); // Correction from video, thanks schuhmann.
+    
+    sleep_us(130);
+
+}
+
+void NRF24::sendMessage(char *msg){
+    uint8_t reg = 0b10100000;
+    csnLow();
+    spi_write_blocking(port, &reg,1);
+    spi_write_blocking(port, (uint8_t*)msg, 32);
+    csnHigh();
+
+    ceHigh();
+    sleep_us(300); // 300us is needed to reliably send messages
+    ceLow();
+}
+void NRF24::receiveMessage(char *msg){
+    uint8_t cmd = 0b01100001;   
+    csnLow();
+    spi_write_blocking(port, &cmd, 1);
+
+    spi_read_blocking(port,0xff, (uint8_t*)msg,32);
+    csnHigh();
+
+}
+
+uint8_t NRF24::newMessage(){
+    uint8_t fifo_status = readReg(0x17);
+
+    return !(0x00000001 & fifo_status);
+}
+
+void NRF24::setChannel(uint8_t ch){
+    writeReg(5, ch); // channel.
+}
+
+void NRF24::setRXName(char *name){
+    if( strlen(name) != 5) return;
+    writeReg(0x0a, (uint8_t*)name,5);
+}
+
+void NRF24::setTXName(char *name){
+    if( strlen(name) != 5) return;
+    writeReg(0x10, (uint8_t*)name,5);
+}
---
+++ RF/src/main.cpp
@@ -0,0 +1,42 @@
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "NRF24.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    NRF24 nrf(spi1, 21, 22); // Integer values are pins for CS and CE respectively
+    nrf.config();
+    nrf.modeRX(); // Recieving Mode as default
+
+    gpio_init(15); // Initialise the LED
+    gpio_set_dir(25,1); // Sets the direction as output from the Pico
+
+    gpio_init(14); // Initialise On button
+    gpio_set_dir(14,1);
+
+    char TXBuffer[32] {0}; // The Transmission Buffer
+    char RXBuffer[32] {0}; // The Recieve Buffer
+
+    gpio_put(15,1); // Test LED works
+    while(1) {
+        if (!gpio_get(14)) { // If the On button is pressed
+            nrf.modeTX(); // Switch to TX mode
+            sprintf(TXBuffer, "John");
+            nrf.sendMessage(TXBuffer); // Send "On"
+            nrf.modeRX(); // Switch back to RX
+        }
+
+        if (nrf.newMessage() == 1) { // If new message recieved
+            nrf.receiveMessage(RXBuffer);
+            if(strcmp(RXBuffer, "John")) { // Check if the message is John
+                gpio_put(15,0); // If so, turn the light off for 1 second
+                sleep_ms(1000);
+                gpio_put(15,1); // Turn the light on
+            }
+        }
+    }
+    return 0;
+}
+
