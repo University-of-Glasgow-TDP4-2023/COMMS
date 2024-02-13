020ece17ae9cae655517039606ca25af6a2876ac
donnchadofmunster <alexander.lake29@gmail.com>
2024-02-13 12:50:13 +0000

    Added commands to send (and turn on the LED)

--- RF/src/main.cpp
+++ RF/src/main.cpp
@@ -4,17 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// #include <stdio.h>
-// #include "pico/stdlib.h"
-
-// int main() {
-//     stdio_init_all();
-//     while (true) {
-//         printf("Hello, world!\n");
-//         sleep_ms(1000);
-//     }
-// }
-
 int main(){
     stdio_init_all();
     NRF24 nrf(spi1, 21, 22); // Integer values are pins for CS and CE respectively
@@ -24,23 +13,30 @@ int main(){
     gpio_init(15); // Initialise the LED
     gpio_set_dir(15,1); // Sets the direction as output from the Pico
     gpio_init(14); // Initialise On button
-    gpio_set_dir(14,1);
+    gpio_set_dir(14,0);
 
     char TXBuffer[32] {0}; // The Transmission Buffer
     char RXBuffer[32] {0}; // The Recieve Buffer
+    char command;
 
     gpio_put(15,0); // Test LED works
     while(1) {
-        if (!gpio_get(14)) { // If the On button is pressed
+        command = getchar();
+        if (command == '1') { // If command = 1
+            printf(" LED turning on!!!!!!!\n");
+            gpio_put(15,1); // If so, turn the light off for 3 seconds
+            sleep_ms(3000);
+            gpio_put(15,0); // Turn the light on
+            sleep_ms(3000);
             nrf.modeTX(); // Switch to TX mode
-            sprintf(TXBuffer, "John");
+            sprintf(TXBuffer, "1");
             nrf.sendMessage(TXBuffer); // Send "On"
             nrf.modeRX(); // Switch back to RX
         }
 
         if (nrf.newMessage() == 1) { // If new message recieved
             nrf.receiveMessage(RXBuffer);
-            strcpy(RXBuffer, "John");
+            strcpy(RXBuffer, "Johnny");
             if(strcmp(RXBuffer, "John") == 0) { // Check if the message is John
                 printf(RXBuffer,"\n");
                 printf(" Thomas\n");
