31d7f93c72cc84bfa9fc498f5a2ccd0efc7e526c
donnchadofmunster <alexander.lake29@gmail.com>
2024-02-27 11:28:42 +0000

    Alternative TX code stored for later use

--- RF/src/NRF24.cpp
+++ RF/src/NRF24.cpp
@@ -5,132 +5,183 @@
 #include <pico/time.h>
 
 
-NRF24::NRF24(spi_inst_t *port, uint16_t csn, uint16_t ce)
+#include "NRF24.h"
+#include <string.h>
+
+
+NRF24::NRF24(    spi_inst_t *spi, uint16_t csn, uint16_t ce  )
 {
-    this->port = port;
+    this->spi = spi;
     this->csn = csn;
     this->ce = ce;
-
-    spi_init(this->port, 1000000 );
-    gpio_set_function(18, GPIO_FUNC_SPI); // Serial Clock
-    gpio_set_function(19, GPIO_FUNC_SPI); // Serial Data Into NRF
-    gpio_set_function(16, GPIO_FUNC_SPI); // Serial Data Out of NRF
+    
+    // This init should be defined externally by the user.
+    spi_init(this->spi, 8000000);
+    gpio_set_function(18,GPIO_FUNC_SPI);
+    gpio_set_function(19,GPIO_FUNC_SPI);
+    gpio_set_function(16,GPIO_FUNC_SPI);
+    // spi end.
 
     gpio_init(csn);
     gpio_init(ce);
 
-    gpio_set_dir(csn, 1);
-    gpio_set_dir(ce, 1);
+    gpio_set_dir(csn,1);
+    gpio_set_dir(ce,1);
 
     ceLow();
     csnHigh();
-
 }
 
 NRF24::~NRF24()
 {
 }
 
-uint8_t NRF24::readReg(uint8_t reg){
+uint8_t NRF24::readReg(uint8_t reg)
+{
+    reg = 0b00011111 & reg;
     uint8_t result = 0;
-    reg = ( 0b00011111 & reg);
     csnLow();
-    spi_write_blocking(port, &reg,1);
-    spi_read_blocking(port, 0xff,&result, 1);
+    spi_write_blocking(spi,&reg,1);
+    spi_read_blocking(spi,0xff,&result,1);
     csnHigh();
 
     return result;
 }
-
-void NRF24::writeReg(uint8_t reg, uint8_t data){
-    writeReg(reg, &data, 1);
-}
-void NRF24::writeReg(uint8_t reg, uint8_t *data, uint8_t size){
-    reg = 0b00100000 | ( 0b00011111 & reg);
+void NRF24::writeCommand(uint8_t cmd)
+{
     csnLow();
-    spi_write_blocking(port, &reg, 1);
-    spi_write_blocking(port, (uint8_t*)data, size);
+    spi_write_blocking(spi, &cmd, 1);
     csnHigh();
 }
+void NRF24::writeReg(uint8_t reg, uint8_t data)
+{
+    writeReg(reg, &data,1);
+}
+void NRF24::writeReg(uint8_t reg, uint8_t *data, uint8_t size)
+{
+    //     Write bit|  Mask bits
+    reg  = 0b00100000 | (0b00011111 & reg);
+    csnLow();
+    spi_write_blocking(spi, &reg,1);
+    spi_write_blocking(spi,(uint8_t*)data,size);
+    csnHigh();
 
-void NRF24::config(){
+}
+void NRF24::enableAck(uint8_t ack)
+{
+    writeReg(1,ack);
+}
+void NRF24::config(uint8_t *address, uint8_t channel,uint8_t messageLen)
+{
+    //channel = 2;
+    this->channel = channel;
+    this->messageLen = messageLen;
     csnHigh();
     ceLow();
     sleep_ms(11);
 
-    writeReg(0, 0b00001010); // config.
+    writeReg(0,0b00001110); // config CRC, 2Bytes CRD, Powerup.
     sleep_us(1500);
 
-    writeReg(1,0); // no ack.
+    writeReg(1,0b0); // disable ack.
 
-    writeReg(5, 60); // channel.
+    writeReg(3, 0b00000011); // 5 byte RX/TX address.
 
-    writeReg(0x0a, (uint8_t*)"gyroc",5);
-    writeReg(0x10, (uint8_t*)"gyroc",5);
+    writeReg(4, 0b00000000); // 250us AutoRetrans delay.
 
-    writeReg(0x11, 32);
+    writeReg(5, channel); // channel.
 
+    writeReg(6, 0b00001110); // 2Mbs, 0dBm... max power.
+
+    writeReg(0x0a,(uint8_t*)address,5);
+    writeReg(0x10,(uint8_t*)address,5);
     
-
+    writeReg(0x11, messageLen); // message Length.
 }
 
-void NRF24::modeTX(){
-    uint8_t reg = readReg(0);
-    reg &= ~(1<<0);
-    writeReg(0, reg);
-    
-    ceLow(); // Correction from video, thansk schuhmann.
-    
-    sleep_us(130);
-}
-void NRF24::modeRX(){
-    uint8_t reg = readReg(0);
-    reg |= (1<<0);
-    writeReg(0, reg);
-    
-    ceHigh(); // Correction from video, thanks schuhmann.
-    
-    sleep_us(130);
 
+void NRF24::modeTX()
+{
+    
+    uint8_t config = readReg(0);
+    config &= ~(1<<0); // clear PRIM_RX bit.
+    writeReg(0,config);
+    ceLow();
+
+    sleep_us(130); // 130us settling time.
 }
 
-void NRF24::sendMessage(char *msg){
-    uint8_t reg = 0b10100000;
+void NRF24::modeRX()
+{
+    uint8_t config = readReg(0);
+    config |= (1<<0); // set PRIM_RX bit.
+    writeReg(0,config);
+    ceHigh();
+    sleep_us(130); // 130us settling time.    
+ }
+uint8_t NRF24::newMessage()
+{
+    return ((readRegister(7) & 0b00001110) < 11);
+
+}
+void NRF24::sendMessage(uint8_t *data)
+{
+    uint8_t flush_tx = 0b11100001;
+    uint8_t tx_payload = 0b10100000;
+    uint8_t status = readReg(7);
+
+    uint8_t buffer[32] = {0};
+    memcpy(buffer,data,messageLen);
+
+    if( status & 1)
+    {
+    	writeCommand(flush_tx);
+    }
+    if( status & 0b00110000)
+        writeReg(7,0b00110000); // Clear Max_RT bit and dat send(TX_DS).
+
+
     csnLow();
-    spi_write_blocking(port, &reg,1);
-    spi_write_blocking(port, (uint8_t*)msg, 32);
+    spi_write_blocking(spi,&tx_payload,1);
+    spi_write_blocking(spi,buffer, messageLen);
     csnHigh();
 
     ceHigh();
-    sleep_us(300); // 300us is needed to reliably send messages
+
+    while( (readReg(7) & 0b00110000) == 0 ) 
+    {
+        // wait until data sent or max rt.
+    }
+
     ceLow();
+    writeReg(7,0b00110000);
+    
+
+    uint8_t observer = readReg(8);
+    if( observer & 0b11110000)
+    {
+        packetsLost += (observer>>4); // keep track of packet lost.
+        writeReg(5,channel); // clear PLOST_CNT by writing channel.
+    }
+
 }
-void NRF24::receiveMessage(char *msg){
-    uint8_t cmd = 0b01100001;   
+
+void NRF24::getMessage(uint8_t *buffer)
+{
+    uint8_t rx_payload = 0b01100001;
+
     csnLow();
-    spi_write_blocking(port, &cmd, 1);
-
-    spi_read_blocking(port,0xff, (uint8_t*)msg,32);
+    spi_write_blocking(spi,&rx_payload,1);
+    spi_read_blocking(spi,0xff,(uint8_t*)buffer,messageLen);
     csnHigh();
+    
+    writeReg(7, 0b01000000);
 
 }
 
-uint8_t NRF24::newMessage(){
-    uint8_t fifo_status = readReg(0x17);
-
-    return !(0x00000001 & fifo_status);
+uint8_t NRF24::readRegister(uint8_t reg)
+//uint8_t NRF24::readRegister(uint8_t reg)
+{
+    return readReg(reg);
 }
 
-void NRF24::setChannel(uint8_t ch){
-    writeReg(5, ch); // channel.
-}
-
-void NRF24::setRXName(char *name){
-    if( strlen(name) != 5) return;
-    writeReg(0x0a, (uint8_t*)name,5);
-}
-
-void NRF24::setTXName(char *name){
-    if( strlen(name) != 5) return;
-    writeReg(0x10, (uint8_t*)name,5);
-}
--- RF/src/main.cpp
+++ RF/src/main.cpp
@@ -1,52 +1,91 @@
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
 #include "NRF24.h"
-#include <stdio.h>
-#include <string.h>
+#include "HM11.H"
+#include "stdio.h"
 
-int main(){
-    stdio_init_all();
-    NRF24 nrf(spi0, 21, 22); // Integer values are pins for CS and CE respectively
-    nrf.config();
-    nrf.modeRX(); // Recieving Mode as default
+int main()
+{
 
-    gpio_init(15); // Initialise the LED
-    gpio_set_dir(15,1); // Sets the direction as output from the Pico
-    gpio_init(14); // Initialise On button
-    gpio_set_dir(14,0);
+    typedef struct _nrf{
+        spi_inst_t* spi;
+        uint8_t csn;
+        uint8_t ce;
 
-    char TXBuffer[32] {0}; // The Transmission Buffer
-    char RXBuffer[32] {0}; // The Recieve Buffer
-    char command;
+    }NRF;
 
-    gpio_put(15,0); // Test LED works
-    while(1) {
-        command = getchar();
-        if (command == '1') { // If command = 1
-            printf(" LED turning on!!!!!!!\n");
-            gpio_put(15,1); // If so, turn the light off for 3 seconds
-            sleep_ms(3000);
-            gpio_put(15,0); // Turn the light on
-            sleep_ms(3000);
-            nrf.modeTX(); // Switch to TX mode
-            sprintf(TXBuffer, "1");
-            printf(TXBuffer,"\n");
-            nrf.sendMessage(TXBuffer); // Send the buffer
-            nrf.modeRX(); // Switch back to RX
-        }
+        gpio_init(15); // led
 
-        if (nrf.newMessage() == 1) { // If new message recieved
-            nrf.receiveMessage(RXBuffer);
-            // strcpy(RXBuffer, "Johnny");
-            if(strcmp(RXBuffer, "1") == 0) { // Check if the message is John
-                printf(RXBuffer,"\n");
-                printf(" Thomas\n");
-                gpio_put(15,1); // If so, turn the light off for 1 second
-                sleep_ms(3000);
-                gpio_put(15,0); // Turn the light on
-                sleep_ms(3000);
-            }
-        }
-    }
+        gpio_set_dir(15,1);
+
+        sleep_ms(100); // give time for things to settle.
+
+        NRF24 nrf = NRF24(spi1,21,22);
+        nrf.config((uint8_t*)"gyroc",2,5); // Name=gyroc, channel=2,messagSize=10
+
+        nrf.modeTX(); // <---- Set as transmitter.
+
+        char message[10];
+        uint16_t counter = 0;
+
+        while(1)  
+        {
+            sprintf(message,"%d",counter++);
+            nrf.sendMessage((uint8_t*)message);
+            sleep_ms(20);
+        } 
+        
     return 0;
 }
+
+// #include "pico/stdlib.h"
+// #include "hardware/spi.h"
+// #include "NRF24.h"
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(){
+//     stdio_init_all();
+//     NRF24 nrf(spi0, 21, 22); // Integer values are pins for CS and CE respectively
+//     nrf.config();
+//     nrf.modeRX(); // Recieving Mode as default
+
+//     gpio_init(15); // Initialise the LED
+//     gpio_set_dir(15,1); // Sets the direction as output from the Pico
+//     gpio_init(14); // Initialise On button
+//     gpio_set_dir(14,0);
+
+//     char TXBuffer[32] {0}; // The Transmission Buffer
+//     char RXBuffer[32] {0}; // The Recieve Buffer
+//     char command;
+
+//     gpio_put(15,0); // Test LED works
+//     while(1) {
+//         command = getchar();
+//         if (command == '1') { // If command = 1
+//             printf(" LED turning on!!!!!!!\n");
+//             gpio_put(15,1); // If so, turn the light off for 3 seconds
+//             sleep_ms(3000);
+//             gpio_put(15,0); // Turn the light on
+//             sleep_ms(3000);
+//             nrf.modeTX(); // Switch to TX mode
+//             sprintf(TXBuffer, "1");
+//             printf(TXBuffer,"\n");
+//             nrf.sendMessage(TXBuffer); // Send the buffer
+//             nrf.modeRX(); // Switch back to RX
+//         }
+
+//         if (nrf.newMessage() == 1) { // If new message recieved
+//             nrf.receiveMessage(RXBuffer);
+//             // strcpy(RXBuffer, "Johnny");
+//             if(strcmp(RXBuffer, "1") == 0) { // Check if the message is John
+//                 printf(RXBuffer,"\n");
+//                 printf(" Thomas\n");
+//                 gpio_put(15,1); // If so, turn the light off for 1 second
+//                 sleep_ms(3000);
+//                 gpio_put(15,0); // Turn the light on
+//                 sleep_ms(3000);
+//             }
+//         }
+//     }
+//     return 0;
+// }
